21a213b095dc360d9c80d3344329de054154cf60
Toby <newtobycm@gmail.com>
2023-08-04 23:05:36 -0700

    -E doesn't work on doas

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -1,6 +1,6 @@
 sudo apt install curl -y
 
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo bash - &&\
 sudo apt install -y nodejs
 
 [[ ! -f ./install-pm2.sh ]] || ./install-pm2.sh

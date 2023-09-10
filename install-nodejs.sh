ef86268e9613f209ff9b4d1c64c45d53ead58bc8
Toby <62174797+tobycm@users.noreply.github.com>
2023-09-09 19:16:57 -0700

    Update new way of installing nodejs
    Signed-off-by: Toby <62174797+tobycm@users.noreply.github.com>

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -1,6 +1,13 @@
 sudo apt install curl -y
 
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo bash - &&\
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+sudo apt update
 sudo apt install -y nodejs
 
 [[ ! -f ./install-pm2.sh ]] || ./install-pm2.sh

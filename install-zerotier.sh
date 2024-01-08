9a178596a65e5239204b8d1f91771da6b0f63aa5
Toby Cm <toby@vsus.app>
2024-01-07 20:08:03 -0800

    Update install-zerotier.sh

--- install-zerotier.sh
+++ install-zerotier.sh
@@ -2,5 +2,3 @@ sudo apt-get install curl gpg -y
 
 curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && \
 if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
-
-sudo zerotier-cli join a0cbf4b62a201e39
